a1702e0d86421429d1375e2d6e11c0f86a9784ab
The Haive <vadim.adiyatov@gmail.com>
2026-01-08 19:28:58 +0500

    Update funtional

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
-import { getPostsData } from './thumbnails.js';
-import { initUploadPictureModule } from './upload-picture.js';
+import {getData} from './api.js'; 
+import {getItems} from './data.js';
+import {initGallery} from './gallery.js';
+import {debounce, showAlert} from './utils.js';
+import {changeFilter, showFilter} from './filter.js';
+import {setState, getState} from './state.js';
+import {renderSmallItems} from './small-items.js';
+import {hideFormUpload, initFormUpload} from './form-upload.js';
+import {initValidation} from './validation.js';
 
-getPostsData();
-initUploadPictureModule();
+const RENDER_DELAY = 500;
+setState(getItems());
+try {
+  renderSmallItems(getState());
+  initGallery(getState());
+  changeFilter(debounce(() => renderSmallItems(getState()), RENDER_DELAY));
+  showFilter();
+} catch (err) {
+  showAlert(err.message);
+}
+initFormUpload(initValidation, hideFormUpload);
+
+
+getData()
+  .then((items) => {
+    setState(items);
+  })
+  .then(() => {
+    renderSmallItems(getState());
+    initGallery(getState());
+    changeFilter(debounce(() => renderSmallItems(getState()), RENDER_DELAY));
+    showFilter();
+  })
+  .catch((err) => {
+    showAlert(err.message);
+  });
+
+initFormUpload(initValidation, hideFormUpload);
--- js/utils.js
+++ js/utils.js
@@ -1,15 +1,45 @@
-const removeElements = (elements) => elements.forEach((element) => element.remove());
+const ALERT_SHOW_TIME = 5000;
 
-const shuffleElements = (elements) => {
-  for (let i = elements.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [elements[i], elements[j]] = [elements[j], elements[i]];
-  }
-
-  return elements;
+const alertStyles = {
+  zIndex : '100',
+  position : 'absolute',
+  left : '0',
+  top : '0',
+  right : '0',
+  padding : '10px 3px',
+  fontSize : '30px',
+  lineHeight : '36px',
+  textAlign : 'center',
+  backgroundColor : '#232321',
+  color: '#ffffff',
 };
 
-const debounce = (callback, timeoutDelay) => {
+export const checkLength = (array, maxLength) => array.length <= maxLength;
+
+export const checkRepeats = (array) => {
+  const itemsInUpperCase = array.map((item) => item.toUpperCase());
+  const arrayNoRepeats = new Set(itemsInUpperCase);
+  return arrayNoRepeats.size === itemsInUpperCase.length;
+};
+
+export const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export const isEnterKey = (evt) => evt.key === 'Enter';
+
+export const removeLastCharacter = (string) => string ? string.slice(0, -1) : string;
+
+export const showAlert = (message) => {
+  const alertContainerElement = document.createElement('div');
+  Object.assign(alertContainerElement.style, alertStyles);
+  alertContainerElement.textContent = message;
+  document.body.append(alertContainerElement);
+
+  setTimeout(() => {
+    alertContainerElement.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+export const debounce = (callback, timeoutDelay = 500) => {
   let timeoutId;
 
   return (...rest) => {
@@ -18,4 +48,42 @@ const debounce = (callback, timeoutDelay) => {
   };
 };
 
-export { removeElements, shuffleElements, debounce };
+//Следующие 4 функции для работы с муковыми данными
+
+//Функция получения уникального идентификатора (автоинкремент)
+export const createIdGenerator = (start = 0) => {
+  let lastGeneratedId = start;
+
+  return function() {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+};
+
+//Функция получения уникального идентификатора (начинает сначала при достижении заданного значения)
+export const createCircleGenerator = (maxCount) => {
+  let lastGeneratedId = 0;
+
+  return function() {
+    if(lastGeneratedId >= maxCount) {
+      lastGeneratedId = 0;
+      return lastGeneratedId++;
+    }
+    return lastGeneratedId++;
+  };
+};
+
+//Функция получения случайного значения в заданных пределах
+export const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+//Функция получения случайного элемента массива
+export const getRandomArrayElement = (elements) => (
+  elements[getRandomInteger(0, elements.length - 1)]
+);
+
